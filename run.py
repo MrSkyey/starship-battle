12895580c2e2dc7641d3617c968b34cfc1e3a902
Clarke Jones <mrskyey@proton.me>
2024-11-24 13:32:42 +0000

    Create python code

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
